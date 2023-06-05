2449f2c65bd70e8a4024207a9fa66e876d2daf13
vanesagh <vanesag@chalmers.se>
2023-06-05 11:09:12 -0600

    Changed to 3003

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use("/products", ProductRoutes);
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3003;
 
 const connectDB = async () => {
     try {
